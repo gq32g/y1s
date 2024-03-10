a1780c9920ad66ed3210c905652cc88e2f9f5906
gq32g <75679327+gq32g@users.noreply.github.com>
2024-03-10 20:27:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -39,8 +39,8 @@
 
 #echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 
-git clone https://github.com/nickilchen/luci-app-socat.git package/luci-app-socat
-git clone https://github.com/rufengsuixing/luci-app-zerotier.git package/luci-app-zerotier
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+#git clone https://github.com/nickilchen/luci-app-socat.git package/luci-app-socat
+#git clone https://github.com/rufengsuixing/luci-app-zerotier.git package/luci-app-zerotier
+#git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 
 
