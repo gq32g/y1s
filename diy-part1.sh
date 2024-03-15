21dd6cebfff546a0d220f7961a75e118ea84e55d
gq32g <75679327+gq32g@users.noreply.github.com>
2024-03-15 18:45:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -37,7 +37,7 @@
 #git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git
 #或者添加下面代码到feeds.conf.default文件
 
-echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
+#echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 
 #git clone https://github.com/nickilchen/luci-app-socat.git package/luci-app-socat
 #git clone https://github.com/rufengsuixing/luci-app-zerotier.git package/luci-app-zerotier
